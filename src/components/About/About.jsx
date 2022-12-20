57d574f288fdcebfc00ab9e8b88bcce081e017a6
kaushalluffa <webdevelopment852@gmail.com>
2022-12-19 20:16:43 -0500

    about section html created partial

---
+++ src/components/About/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about__wrapper">
+        <div className="col-1">
+          <h4 className="col-1__title">A BIT ABOUT ME</h4>
+          <p className="col-1__description">
+            I am a front-end developer who is passionate about creating
+            <span className="col-1__description--bold">
+              beautiful and functional digital experiences. Besides development,
+              I love games, travelling and music.
+            </span>
+          </p>
+        </div>
+        <div className="col-2">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;

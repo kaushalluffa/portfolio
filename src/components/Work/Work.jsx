0d3d797021eee0fc6bf83ec2afdfd6ad96ea2ebb
kaushalluffa <webdevelopment852@gmail.com>
2022-12-19 20:11:08 -0500

    work section html created

---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Work = () => {
+  return (
+    <div className="work">
+      <h2 className="work__title">
+        Selected <span className="work__title--bolded">Works</span>
+      </h2>
+      <div className="work__cards">
+        <div className="work__cards--card">
+          <div className="col-1">
+            <div className="col-1__img">
+              <img src="" alt="project__img" />
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="col-2__content">
+              <div className="col-2__content--title-description">
+                <h4 className="content__title">REAL ESTATE TEMPLATE</h4>
+                <p className="content__description">
+                  Lorem ipsum, dolor sit amet consectetur{" "}
+                  <span className="content__description--bold">
+                    adipisicing elit. Quo sed sequi soluta dolor! Repellendus.
+                  </span>
+                </p>
+              </div>
+              <div className="col-2__content--stack">
+                ReactJS, NodeJS, MongoDB, ExpressJS
+              </div>
+            </div>
+            <Link to="/" className="col-2__btn">
+              View Work <FontAwesomeIcon icon="fa-solid fa-arrow-right" />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;

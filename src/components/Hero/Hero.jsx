0a7fb35b91ab37f92ed5de676be7360de3192b54
kaushalluffa <webdevelopment852@gmail.com>
2022-12-21 21:02:26 -0500

    desktop css for hero section complete

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,19 @@
-import downArrow from "../../assets/svg/down-arrow.svg";
-import React from "react";
 
+import { FaArrowDown } from "react-icons/fa";
+import React from "react";
+import "./Hero.scss";
 const Hero = () => {
   return (
-    <div className="hero">
+    <section className="hero">
       <h1 className="hero__content">
-        I'm a front-end developer specializing in{" "}
-        <span className="hero__content--bold">UI/ReactJS</span> and
-        <span className="hero__content--bold">Functional Web Development</span>
+        I'm a front-end developer specializing in
+        <span className="hero__content--bold"> UI/ReactJS</span> and
+        <span className="hero__content--bold"> Functional Web Development</span>
       </h1>
       <div className="hero__icon">
-        <span>
-          <img src={downArrow} alt="down arrow" />
-        </span>
+        <FaArrowDown />
       </div>
-    </div>
+    </section>
   );
 };
 

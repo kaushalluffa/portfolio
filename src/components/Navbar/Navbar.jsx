719dcd686f3d5b9d345c2742858dc6bf766dae1e
kaushalluffa <webdevelopment852@gmail.com>
2022-12-21 20:41:11 -0500

    desktop css complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import './Navbar.scss'
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="navbar__logo">Kaushal</div>
         <div className="navbar__navitems">
             <Link to="/">HOME</Link>
-            <Link to="/work">WORK</Link>
-            <Link to="/about">ABOUT</Link>
+            <Link to="/">WORK</Link>
+            <Link to="/">ABOUT</Link>
         </div>
     </div>
   )

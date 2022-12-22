16f3341e4d510d9cdf11a8c3e183e9550221f3a5
kaushalluffa <webdevelopment852@gmail.com>
2022-12-21 21:50:49 -0500

    work section desktop css complete

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,11 +1,11 @@
-import rightArrow from '../../assets/svg/right-arrow.svg'
+import { FaArrowRight } from "react-icons/fa";
 import React from "react";
 import { Link } from "react-router-dom";
-import project from '../../assets/images/webdev.jpeg'
-
+import project from "../../assets/images/webdev.jpeg";
+import "./Work.scss";
 const Work = () => {
   return (
-    <div className="work">
+    <section className="work">
       <h2 className="work__title">
         Selected <span className="work__title--bolded">Works</span>
       </h2>
@@ -21,7 +21,7 @@ const Work = () => {
               <div className="col-2__content--title-description">
                 <h4 className="content__title">REAL ESTATE TEMPLATE</h4>
                 <p className="content__description">
-                  Lorem ipsum, dolor sit amet consectetur
+                  Lorem ipsum, dolor sit amet consectetur{" "}
                   <span className="content__description--bold">
                     adipisicing elit. Quo sed sequi soluta dolor! Repellendus.
                   </span>
@@ -32,12 +32,65 @@ const Work = () => {
               </div>
             </div>
             <Link to="/" className="col-2__btn">
-              View Work <span><img src={rightArrow} alt="right arrow"/></span>
+              View Work <FaArrowRight />
             </Link>
           </div>
         </div>
+        <div className="work__cards--card">
+          <div className="col-1">
+            <div className="col-1__img">
+              <img src={project} alt="project__img" />
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="col-2__content">
+              <div className="col-2__content--title-description">
+                <h4 className="content__title">REAL ESTATE TEMPLATE</h4>
+                <p className="content__description">
+                  Lorem ipsum, dolor sit amet consectetur{" "}
+                  <span className="content__description--bold">
+                    adipisicing elit. Quo sed sequi soluta dolor! Repellendus.
+                  </span>
+                </p>
+              </div>
+              <div className="col-2__content--stack">
+                ReactJS, NodeJS, MongoDB, ExpressJS
+              </div>
+            </div>
+            <Link to="/" className="col-2__btn">
+              View Work <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+        <div className="work__cards--card">
+          <div className="col-1">
+            <div className="col-1__img">
+              <img src={project} alt="project__img" />
+            </div>
+          </div>
+          <div className="col-2">
+            <div className="col-2__content">
+              <div className="col-2__content--title-description">
+                <h4 className="content__title">REAL ESTATE TEMPLATE</h4>
+                <p className="content__description">
+                  Lorem ipsum, dolor sit amet consectetur{" "}
+                  <span className="content__description--bold">
+                    adipisicing elit. Quo sed sequi soluta dolor! Repellendus.
+                  </span>
+                </p>
+              </div>
+              <div className="col-2__content--stack">
+                ReactJS, NodeJS, MongoDB, ExpressJS
+              </div>
+            </div>
+            <Link to="/" className="col-2__btn">
+              View Work <FaArrowRight />
+            </Link>
+          </div>
+        </div>
+       
       </div>
-    </div>
+    </section>
   );
 };
 

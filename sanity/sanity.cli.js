25ee3fe17a70c852714a9e478dd5ef683ea12184
kaushalluffa <webdevelopment852@gmail.com>
2022-12-23 21:03:51 -0500

    removed sanity folder from git

--- sanity/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'rf5nnsz3',
-    dataset: 'production'
-  }
-})
--- sanity/schemas/index.js
+++
@@ -1,2 +0,0 @@
-import project from './projects'
-export const schemaTypes = [project]

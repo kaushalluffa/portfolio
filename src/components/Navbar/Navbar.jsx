0fff3a1271cfc16371abd8fd23147427b7a311db
kaushalluffa <webdevelopment852@gmail.com>
2022-12-23 22:17:06 -0500

    mobile styling for navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Navbar.scss'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import "./Navbar.scss";
 const Navbar = () => {
+  const [show, setShow] = useState(false)
+  function openMenu(){
+    setShow(!show)
+  }
   return (
-    <div className='navbar'>
-        <div className="navbar__logo"><span className="text">Kaushal</span></div>
-        <div className="navbar__navitems">
-            <Link to="/">HOME</Link>
-            <Link to="/">WORK</Link>
-            <Link to="/">ABOUT</Link>
+    <>
+      <div className="navbar">
+        <div className="navbar__logo">
+          <span className="text">Kaushal</span>
         </div>
-    </div>
-  )
-}
+        <div className="navbar__navitems">
+          <Link to="/">HOME</Link>
+          <Link to="/">WORK</Link>
+          <Link to="/">ABOUT</Link>
+        </div>
+        <div className="bars" onClick={openMenu}>
+          <FaBars />
+        </div>
+      </div>
+      <div className={`menu ${show ? 'show': ''}`}>
+        <Link to="/">HOME</Link>
+        <Link to="/">WORK</Link>
+        <Link to="/">ABOUT</Link>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
